e34bcd120403118cd5c89c9254f0083290c24ffd
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-05-15 16:25:15 +0530

    Initial Commit

---
+++ Snail/Snail.h
@@ -0,0 +1,18 @@
+//
+//  Snail.h
+//  Snail
+//
+//  Created by Avijeet Pandey on 15/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Snail.
+FOUNDATION_EXPORT double SnailVersionNumber;
+
+//! Project version string for Snail.
+FOUNDATION_EXPORT const unsigned char SnailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Snail/PublicHeader.h>
+
+
